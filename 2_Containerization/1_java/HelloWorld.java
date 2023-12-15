5b979a51fbf82fa660aa072b2cc78e4f10315a5c
Nivasan <58383171+NivasanV@users.noreply.github.com>
2023-12-15 04:49:43 +0000

    java

--- 2_Containerization/1_java/HelloWorld.java
+++ 2_Containerization/1_java/HelloWorld.java
@@ -1,5 +1,5 @@
-pubic class HelloWorld{
-    pubic staic void main(String []args){
+public class HelloWorld{
+    public static void main(String []args){
         System.out.println("Hello world docker");
     }
 }
